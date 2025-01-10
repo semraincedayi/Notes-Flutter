6e80ddfb8b47be283f2a47faf5a6173139e8edc8
semraincedayi <semraincedayi@hotmail.com>
2025-01-10 11:10:46 +0300

    Notes server

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+
+const mongoose= require('mongoose');
+const Note=require('./models/Note');
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+mongoose.connect("mongodb+srv://incedayisemra35:41292@cluster0.9rkeh.mongodb.net/Cluster0").then(function () {
+    
+    app.get("/", function (req,res) {
+        const response={message: "API works!"};
+        res.json(response);
+    });  
+    const noteRouter=require('./routes/Note');
+    app.use("/notes",noteRouter);
+});
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,function(){
+    console.log("server started at PORT:"+PORT);
+
+});
